0e526ac998821ae1ebe6bcf6e4adab55faa7faeb
Edvinn <ealvaeus@kth.se>
2022-04-06 02:24:33 +0200

    setup

--- src/api/data.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios'
-const dataUrl = "http://localhost:5000/data"
-
-export const getCurrentData = () => axios.get(dataUrl)
--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import currentDataReducer from "./currentDataReducer";
-
-const rootReducer = combineReducers({
-    currentData: currentDataReducer
-})
-
-export default rootReducer;
