6ea87570bb8eb723255c53097957cdd64e0feacb
Edvinn <ealvaeus@kth.se>
2022-04-06 21:54:19 +0200

    changed data url to deployed

--- src/api/data.js
+++ src/api/data.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-const url = process.env.PORT ? "http://pollusenseserver.azurewebsites.net/data" : "http://localhost:5000/data"
-// const dataUrl = "http://localhost:5000/data"
-// const deployedUrl = "http://pollusenseserver.azurewebsites.net/"
-export const getCurrentData = () => axios.get(url)
+const devUrl = "http://localhost:5000/data"
+const deployedUrl = "http://pollusenseserver.azurewebsites.net/"
+export const getCurrentData = () => axios.get(deployedUrl)
