5e8b48845abc64985422d9221efe393975e331eb
Edvinn <ealvaeus@kth.se>
2022-04-07 14:30:24 +0200

    test feature branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 
-
+//test
 const App = () => {
   return (
     <BrowserRouter>
