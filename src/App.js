9a79f73ace074c0b4fe3e81ad3333aefc88e4616
Edvinn <ealvaeus@kth.se>
2022-04-07 17:47:20 +0200

    test cypress action

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 
-//test
 const App = () => {
   return (
     <BrowserRouter>
