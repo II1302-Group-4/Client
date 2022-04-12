dbe1e7f6c05f53bce174e32a50f92847af1722da
Parosh Shaways <shaways@kth.se>
2022-04-12 09:57:25 +0200

    Styling

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -6,10 +6,31 @@ const ExampleComponent = () => {
     const currentData = useSelector(state => state.currentData)
 
     return (
-        <>
-            <h1>Pollusense</h1>
-            <button onClick={() => { dispatch(getCurrentData()) }}>Get latest reading with a server call</button>
+        <>  
+        <div>  <p></p></div>
+
+            <div class="container-fluid">
+            <div class="row p-3 mb-2 bg-info text-white">
+            <div class="col col-sm-7"><h3>PolluSense</h3>  </div>
+
+    <div class="col">
+    </div>
+    <div class="col">
+    </div>
+
+
+    <div class="col-auto">
+    <button type="button" class="btn btn-outline-light">Log in</button>
+    </div>
+   
+
+           </div>		 
+           </div>
+           <div>  <p></p></div>
+           <div class="col-12 col-sm-7">
+           <button type="button" class="btn btn-primary btn-lg btn-block"  onClick={() => { dispatch(getCurrentData()) }}>Get latest reading with a server call </button>
             {currentData.loadingCurrentData ? <h2>Loading data...</h2> : <h2>{currentData.currentData}</h2>}
+            </div>
         </>
     )
 }
