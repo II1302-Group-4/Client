9aca137a44049a37ca093e6db7aae004fbf11758
originalpau <pauhua@kth.se>
2022-04-28 19:32:54 +0200

    onlyPassword

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,16 +6,25 @@ const Landing = () => {
             <div className="d-flex flex-column bd-highlight my-5">
                 <img src={test_logo} alt="PolluSense Logo"/>
                 <h2 class="mb-4">The fast and simple way of checking for dangerous VOC levels</h2>
-                <hr class="my-5"/>
+                {/*<hr class="my-5"/>*/}
+                <form class="my-5">
+                    <div class="text-center my-4">
+                        <input type="password" class="form-control formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputPassword" placeholder="*******"/>
+                            <button type="submit" class="btn btn-outline-secondary ">LOGIN</button>
+                    </div>
+                </form>
+                
+            </div>
+
+{/*
                 <form >
                     <div class="form-group text-center my-4">
                         <input type="email" class="form-control formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputEmail" placeholder="name@kth.se"/>
                         <input type="password" class="form-control formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputPassword" placeholder="*******"/>
                         <button type="submit" class="btn btn-outline-secondary">LOGIN</button>
                     </div>
-                </form>
-                
-            </div>
+                </form>          
+*/}
         </>
     )
 }
