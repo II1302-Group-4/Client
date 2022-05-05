42dd52f1bcf636f8d6adbc56661dfb6c5a947431
originalpau <pauhua@kth.se>
2022-05-05 13:44:30 +0200

    CSS fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,13 +5,13 @@ const Landing = () => {
     return (
         <>
             <div className="d-flex flex-column bd-highlight mt-5">
-                <img src={logo} className="img-fluid mx-auto logo" alt="PolluSense Logo" />
-                <h2 className="mb-4 text-center">The fast and simple way of checking for dangerous VOC levels</h2>
-                <hr className="my-5" />
+                <img src={logo} className="mx-auto logo" alt="PolluSense Logo" />
+                <h2 className="my-4 text-center">The fast and simple way of checking for unhealthy VOC and CO2 levels</h2>
+                <hr className="my-4" />
                 <form>
                     <div className="text-center my-4">
-                        <input type="email" className="form-control formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputEmail" placeholder="name@kth.se" />
-                        <input type="password" className="form-control formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputPassword" placeholder="*******" />
+                        <input type="email" className="formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputEmail" placeholder="name@kth.se" />
+                        <input type="password" className="formInput shadow-sm p-2 mb-4 bg-white rounded" id="InputPassword" placeholder="*******" />
                         <Link to="/monitor">
                             <button type="submit" className="btn btn-outline-secondary" id="loginButton">LOGIN</button>
                         </Link>
