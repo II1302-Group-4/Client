a52e1f431fd4e145d1683c7f057173ccff8e0217
originalpau <pauhua@kth.se>
2022-05-16 16:07:36 +0200

    privateRoute_notFixed

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import './style.css'
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import PresentValue from './pages/PresentValue'
 import AboutPage from './pages/AboutPage'
 import HowToPage from './pages/HowToPage'
 import { useSelector } from 'react-redux'
-import { firebaseConnect, isLoaded } from 'react-redux-firebase'
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
 
-  const loggedIn = useSelector(state => !(state.firebase.auth.isEmpty))
+  // const loggedIn = useSelector(state => !(state.firebase.auth.isEmpty))
 
-  const ProtectedRoute = ({ children }) => {
-    if (!loggedIn) {
-      return <Navigate to="/" replace />;
-    }
-    return children;
-  };
+  // const ProtectedRoute = ({ children }) => {
+  //   if (!loggedIn) {
+  //     return <Navigate to="/" replace />;
+  //   }
+  //   return children;
+  // };
 
-    return (
-        <div className="fillWindow">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<LandingPage />} />
-            {loggedIn?
-              <>
-              <Route path="/monitor" element={<PresentValue />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/how-to" element={<HowToPage />} />
-              </>
-              :
-              <></>
-            }
-            
-          </Routes>
-        </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="fillWindow">
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+        <Route exact path="/monitor" element={<PrivateRoute/>}>
+          <Route exact path="/monitor" element={<PresentValue />} />
+        </Route>
+        <Route exact path="/about" element={<PrivateRoute/>}>
+          <Route exact path="/about" element={<AboutPage />} />
+        </Route>
+        <Route exact path="/how-to" element={<PrivateRoute/>}>
+          <Route exact path="/how-to" element={<HowToPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
+)
+
+    // return (
+    //     <div className="fillWindow">
+    //     <BrowserRouter>
+    //       <Routes>
+    //         <Route exact path="/" element={<LandingPage />} />
+    //         {loggedIn?
+    //           <>
+    //           <Route path="/monitor" element={<PresentValue />} />
+    //           <Route path="/about" element={<AboutPage />} />
+    //           <Route path="/how-to" element={<HowToPage />} />
+    //           </>
+    //           : 
+    //           <></>
+    //         }
+    //       </Routes>
+    //     </BrowserRouter>
+    //     </div>
+    // )
   
   // }
 
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { isLoaded, isEmpty } from "react-redux-firebase";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
+// const loggedIn = useSelector(state => !(state.firebase.auth.isEmpty))
+
+// const PrivateRoute = ({ children }) => {
+//   if (!loggedIn) {
+//     return <Navigate to="/" replace />;
+//   }
+//   return children;
+// };
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated or if auth is not
+// yet loaded
+const PrivateRoute = () => {
+  // TODO: Not working, you can still access the not accessible locations
+//   const auth = useSelector(state => state.firebase.auth.uid);
+//   console.log(auth.uid)
+  //const loggedIn = isLoaded(auth) && !isEmpty(auth);
+
+  const auth = getAuth();
+  let loggedIn = onAuthStateChanged(auth, (user) => {
+      if (user) {
+          return true;
+      } else {
+          return false;
+      }
+  })
+
+  
+  return loggedIn? <Outlet/> : <Navigate to="/" />;
+}
+
+export default PrivateRoute;
