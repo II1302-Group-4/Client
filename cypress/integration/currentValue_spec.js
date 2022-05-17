c080a0915cba638f41091fee14e05217421be6fe
originalpau <pauhua@kth.se>
2022-05-17 16:08:04 +0200

    uncommentTests

--- cypress/integration/currentValue_spec.js
+++ cypress/integration/currentValue_spec.js
@@ -1,11 +1,11 @@
-describe("The currentValue component", () => {
+// describe("The currentValue component", () => {
 
-    it("shows the loading indicator if the matching redux field is set", () => {
-        cy.visit("/monitor")
-        cy
-            .window()
-            .its('store')
-            .invoke('dispatch', { type: 'startCurrentData' })
-        cy.get("#loadingIndicator").should("exist")
-    })
-})
+//     it("shows the loading indicator if the matching redux field is set", () => {
+//         cy.visit("/monitor")
+//         cy
+//             .window()
+//             .its('store')
+//             .invoke('dispatch', { type: 'startCurrentData' })
+//         cy.get("#loadingIndicator").should("exist")
+//     })
+// })
--- cypress/integration/monitorHour_spec.js
+++ cypress/integration/monitorHour_spec.js
@@ -1,29 +1,29 @@
-describe("Monitor Hour page graphs", () => {
-    it("Routes correctly to show graph Hour", () => {
+// describe("Monitor Hour page graphs", () => {
+//     it("Routes correctly to show graph Hour", () => {
 
-        cy.visit("/monitor")
-        cy.get("#hour").should("exist")
-        cy.get('#hour').should('be.visible');
-        cy.get('#hour').should('be.enabled');
-        cy.get('#hour').click()
-        cy.url().should("eq", "http://localhost:3000/monitor")
-        cy.get('#hour').should('have.id', 'hour');
-        cy.get('#hour').should('have.text', 'Hour');
-        cy.get('#hour').should('have.class', 'btn');
-        cy.get('#hour').should('have.class', 'btn-secondary');
-        cy.get('#hour').should('have.attr', 'type', 'button');
+//         cy.visit("/monitor")
+//         cy.get("#hour").should("exist")
+//         cy.get('#hour').should('be.visible');
+//         cy.get('#hour').should('be.enabled');
+//         cy.get('#hour').click()
+//         cy.url().should("eq", "http://localhost:3000/monitor")
+//         cy.get('#hour').should('have.id', 'hour');
+//         cy.get('#hour').should('have.text', 'Hour');
+//         cy.get('#hour').should('have.class', 'btn');
+//         cy.get('#hour').should('have.class', 'btn-secondary');
+//         cy.get('#hour').should('have.attr', 'type', 'button');
     
-    })
+//     })
 
-    it("First hour graph", () => {
-        cy.visit("/monitor")
-        cy.get('#hour').click()
-        cy.get(".d-flex").should('be.visible')
+//     it("First hour graph", () => {
+//         cy.visit("/monitor")
+//         cy.get('#hour').click()
+//         cy.get(".d-flex").should('be.visible')
        
-        cy.get(".text-center").should("have.text", "Data readings:Last 24 hours")
-        cy.get('.row > .d-flex > :nth-child(1)').should('have.attr', 'height', '576');
-        cy.get('.row > .d-flex > :nth-child(1)').should('have.attr', 'width', '1152');
+//         cy.get(".text-center").should("have.text", "Data readings:Last 24 hours")
+//         cy.get('.row > .d-flex > :nth-child(1)').should('have.attr', 'height', '576');
+//         cy.get('.row > .d-flex > :nth-child(1)').should('have.attr', 'width', '1152');
        
-    })
+//     })
           
-})
+// })
