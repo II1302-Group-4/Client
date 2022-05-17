17ab10605ba59290b9b8cecbd7952d5a188ae0d4
originalpau <pauhua@kth.se>
2022-05-17 15:48:11 +0200

    updateFirebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4DZ4nGUWqOx7pMtYIMQid-H-7TBpeqSo",
-  authDomain: "pollusense-3c51b.firebaseapp.com",
-  projectId: "pollusense-3c51b",
-  storageBucket: "pollusense-3c51b.appspot.com",
-  messagingSenderId: "514369000464",
-  appId: "1:514369000464:web:3070202934e8915f13e695"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
 // Initialize Firebase
