70dc1545365e5138b521d7b16491ca36e8be0706
originalpau <pauhua@kth.se>
2022-05-18 15:37:40 +0200

    clearUnusedCode

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,15 +1,7 @@
 export const signIn = (email, password) => {
     return (dispatch, getState, { getFirebase }) => {
-        dispatch({ 
-            type: "signinStart" })
-        
         const firebase = getFirebase();
-
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(() => {
-                dispatch({ 
-                    type: "signinSuccess" })
-            })
             .then(() => {
                 window.location.pathname = "/monitor"
             })
@@ -25,10 +17,7 @@ export const signUp = (email, password) => {
     
 
     return (dispatch, getState, { getFirebase }) => {
-        dispatch({ 
-            type: "signupStart" 
-        })
-        
+
         const split = email.split("@")
         if(split[1] !== "kth.se") {
             dispatch({
@@ -39,9 +28,6 @@ export const signUp = (email, password) => {
         const firebase = getFirebase()
         
         firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(() => {
-                dispatch({ type: "signupSuccess" })
-            })
             .then(() => {
                 window.location.pathname = "/monitor"
             })
@@ -54,11 +40,9 @@ export const signUp = (email, password) => {
 
 export const signOut = () => {
     return (dispatch, getState, { getFirebase }) => {
-        dispatch({ type: "signoutStart" })
         const firebase = getFirebase()
         firebase.auth().signOut()
             .then(() => {
-                dispatch({ type: "signoutSuccess" })
                 window.location.pathname = "/"
             })
             .catch(e => {
