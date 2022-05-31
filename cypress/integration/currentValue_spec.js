5faeaa56ec216fa27f5bc7f44c881be53cf400ad
Edvinn <ealvaeus@kth.se>
2022-05-31 16:20:07 +0200

    .

--- cypress/integration/currentValue_spec.js
+++ cypress/integration/currentValue_spec.js
@@ -1,15 +1,38 @@
-// describe("Firebase authentication, successful login", () => {
+describe("Firebase authentication, successful login", () => {
 
-//     //Paulina
-//     it("Login successfully with existing user", () => {
-//         cy.visit("/");
-//         cy.get("#InputEmail").type("test@kth.se");
-//         cy.get("#InputPassword").type("123456");
-//         cy.get("#loginButton").click()
-//         cy.url().should("include", "/monitor")
-//     })
+    //Paulina
+    it("Login successfully with existing user", () => {
+        cy.visit("/");
+        cy.get("#InputEmail").type("test@kth.se");
+        cy.get("#InputPassword").type("123456");
+        cy.get("#loginButton").click()
+        cy.url().should("include", "/monitor")
+    })
 
-//     //Parosh
+    //Parosh
+    it("shows the loading indicator if the matching redux field is set", () => {
+        cy.visit("/monitor")
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'startCurrentData' })
+        cy.get("#loadingIndicator").should("exist")
+    })
+
+    it("Check if error is shown when loading values", () => {
+        cy.visit("/monitor")
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'setCurrentDataError' })
+        cy.get("#tryAgain").should("not.exist")
+        // cy.get("#errorMsg").should("contain", "Test Error")
+    })
+})
+
+
+// describe("The currentValue component", () => {
+
 //     it("shows the loading indicator if the matching redux field is set", () => {
 //         cy.visit("/monitor")
 //         cy
@@ -18,47 +41,24 @@
 //             .invoke('dispatch', { type: 'startCurrentData' })
 //         cy.get("#loadingIndicator").should("exist")
 //     })
-
-//     it("Check if error is shown when loading values", () => {
-//         cy.visit("/monitor")
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'setCurrentDataError'})
-//         cy.get("#tryAgain").should("not.exist")
-//         // cy.get("#errorMsg").should("contain", "Test Error")
-//     })
 // })
 
-
-// // describe("The currentValue component", () => {
-
-// //     it("shows the loading indicator if the matching redux field is set", () => {
-// //         cy.visit("/monitor")
-// //         cy
-// //             .window()
-// //             .its('store')
-// //             .invoke('dispatch', { type: 'startCurrentData' })
-// //         cy.get("#loadingIndicator").should("exist")
-// //     })
-// // })
-
-//  //Cannot be success, because a new account is created in the test.
-//     // it("Register successfully", () => {
-//     //     cy.get("#registerEmail").type("user_test_user@kth.se");
-//     //     cy.get("#decidePassword").type("123456");
-//     //     cy.contains("REGISTER").click()
-//     //     cy.url().should("include", "/monitor")
-//     // })
+ //Cannot be success, because a new account is created in the test.
+    // it("Register successfully", () => {
+    //     cy.get("#registerEmail").type("user_test_user@kth.se");
+    //     cy.get("#decidePassword").type("123456");
+    //     cy.contains("REGISTER").click()
+    //     cy.url().should("include", "/monitor")
+    // })
 
 
-//     //Parosh
-//     // it("can click the login button and go to the correct home path", () => {
-//     //     cy.visit("/")
-//     //     cy.get("#InputEmail").type("hello@kth.se")
-//     //     cy.get("#InputPassword").type("123456")
-//     //     cy.get("#loginButton").click()
-//     //     cy.url().should("eq", "http://localhost:3000/monitor")
-//     // })
+    //Parosh
+    // it("can click the login button and go to the correct home path", () => {
+    //     cy.visit("/")
+    //     cy.get("#InputEmail").type("hello@kth.se")
+    //     cy.get("#InputPassword").type("123456")
+    //     cy.get("#loginButton").click()
+    //     cy.url().should("eq", "http://localhost:3000/monitor")
+    // })
 
-//     //Parosh
+    //Parosh
--- cypress/integration/redux_spec.js
+++ cypress/integration/redux_spec.js
@@ -1,53 +1,53 @@
-// describe("the redux store", () => {
-//     it('can dispatch a startCurrentData action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'startCurrentData' })
-//         cy.window().its('store').invoke('getState').its("currentData.loadingCurrentData").should('eq', true)
-//     })
-//     it('can dispatch a setCurrentData action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'setCurrentData', payload: 5 })
-//         cy.window().its('store').invoke('getState').its("currentData.currentData").should('eq', 5)
+describe("the redux store", () => {
+    it('can dispatch a startCurrentData action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'startCurrentData' })
+        cy.window().its('store').invoke('getState').its("currentData.loadingCurrentData").should('eq', true)
+    })
+    it('can dispatch a setCurrentData action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'setCurrentData', payload: 5 })
+        cy.window().its('store').invoke('getState').its("currentData.currentData").should('eq', 5)
 
-//     })
-//     it('can dispatch a setCurrentDataError action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'setCurrentDataError', payload: "error" })
-//         cy.window().its('store').invoke('getState').its("currentData.currentDataError").should('eq', "error")
-//     })
+    })
+    it('can dispatch a setCurrentDataError action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'setCurrentDataError', payload: "error" })
+        cy.window().its('store').invoke('getState').its("currentData.currentDataError").should('eq', "error")
+    })
 
-//     it('can dispatch a startTwentyFourHoursData action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'startTwentyFourHoursData' })
-//         cy.window().its('store').invoke('getState').its("historicData.loadingTwentyFourHoursData").should('eq', true)
-//     })
-//     it('can dispatch a setTwentyFourHoursData action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'setTwentyFourHoursData', payload: [{ VOC: 1, CO2: 2, time: 3 }] })
-//         cy.window().its('store').invoke('getState').its("historicData.twentyFourHoursData").should('deep.equal', [{ VOC: 1, CO2: 2, time: 3 }])
-//     })
-//     it('can dispatch a setTwentyFourHoursDataError action and update the store', () => {
-//         cy.visit('/')
-//         cy
-//             .window()
-//             .its('store')
-//             .invoke('dispatch', { type: 'setTwentyFourHoursDataError', payload: "error" })
-//         cy.window().its('store').invoke('getState').its("historicData.twentyFourHoursDataError").should('eq', "error")
-//     })
-// })
+    it('can dispatch a startTwentyFourHoursData action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'startTwentyFourHoursData' })
+        cy.window().its('store').invoke('getState').its("historicData.loadingTwentyFourHoursData").should('eq', true)
+    })
+    it('can dispatch a setTwentyFourHoursData action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'setTwentyFourHoursData', payload: [{ VOC: 1, CO2: 2, time: 3 }] })
+        cy.window().its('store').invoke('getState').its("historicData.twentyFourHoursData").should('deep.equal', [{ VOC: 1, CO2: 2, time: 3 }])
+    })
+    it('can dispatch a setTwentyFourHoursDataError action and update the store', () => {
+        cy.visit('/')
+        cy
+            .window()
+            .its('store')
+            .invoke('dispatch', { type: 'setTwentyFourHoursDataError', payload: "error" })
+        cy.window().its('store').invoke('getState').its("historicData.twentyFourHoursDataError").should('eq', "error")
+    })
+})
 
